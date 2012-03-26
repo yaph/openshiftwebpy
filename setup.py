69cca67098dd21c37a423aff03686bfaeffdd665
Ramiro Gomez <web@ramiro.org>
2012-03-26 23:24:46 +0200

    removed non ASCII char from setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='openshiftwebpy',
       version='1.0',
       description='OpenShift web.py example',
-      author='Ramiro Gómez',
+      author='Ramiro Gomez',
       author_email='www@ramiro.org',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['web.py']
