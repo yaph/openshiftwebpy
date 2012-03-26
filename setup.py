fdc00667602c125ffccf0014173513b3152ea563
Ramiro Gomez <web@ramiro.org>
2012-03-27 00:01:29 +0200

    use Jinja2

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='openshiftwebpy',
       author='Ramiro Gomez',
       author_email='www@ramiro.org',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['web.py']
+      install_requires=['web.py', 'Jinja2']
      )
--- wsgi/myapp.py
+++ wsgi/myapp.py
@@ -1,14 +1,29 @@
+import os
 import web
+from jinja2 import Environment,FileSystemLoader
 
 urls = ('/', 'hello')
 app = web.application(urls, globals())
 render = web.template.render('templates/')
 
 
+def render_template(template_name, **context):
+    extensions = context.pop('extensions', [])
+    globals = context.pop('globals', {})
+
+    jinja_env = Environment(
+            loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
+            extensions=extensions,
+            )
+    jinja_env.globals.update(globals)
+
+    #jinja_env.update_template_context(context)
+    return jinja_env.get_template(template_name).render(context)
+
+
 class hello:
     def GET(self):
-        return render.hello()
+        return render_template('hello.html')
 
 
 application = app.wsgifunc()
-
